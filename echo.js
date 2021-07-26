68f9b27d1887a1ee13aeb757f8892f400dd79ef5
Charlie <charlie.davismiller@gmail.com>
2021-07-26 14:04:49 +0100

    echo function output matches specs

---
+++ echo.js
@@ -0,0 +1,28 @@
+const echo = () => {
+  const readline = require('readline');
+  const moment = require('moment');
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const repeatedQuestion = () => {
+    rl.question('Say something:\n', (answer) => {
+      if (answer === 'exit') return rl.close();
+      console.log(
+      `${moment().format('YYYY-MM-DD')} | ${moment().format('LT').slice(0, -2)} | You said: '${answer}'`
+      );
+      repeatedQuestion();
+    });
+  };
+
+  rl.on("close", () => {
+    console.log("Goodbye!");
+    process.exit();
+  });
+
+  repeatedAsyncQuestion();
+};
+
+echo();
